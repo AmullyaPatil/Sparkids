ae2c8d230fab25eee38d95dfa6965e94280d388b
AmullyaPatil <patilamullya@gmail.com>
2024-12-03 00:20:25 +0530

    Abacus Page added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import CoursePage from "./pages/CoursePage";
 import FranchisePage from "./pages/FranchisePage";
 import CareersPage from "./pages/CareersPage";
 import Insight from "./pages/InsightPage";
+import AbacusPage from "./pages/AbacusPage";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/courses" element={<CoursePage />} />
+        <Route path="/abacus" element={<AbacusPage />} />
         <Route path="/franchise" element={<FranchisePage />} />
         <Route path="/careers" element={<CareersPage />} />
         <Route path="/insight" element={<Insight />} />
--- src/pages/CareersPage.jsx
+++ src/pages/CareersPage.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Header from '../components/Header';
-import AbacusOverview from '../components/AbacusOverview';
 import Footer from '../components/Footer';
 export const CareersPage = () => {
     return (
       <div>
         <Header />
-        <AbacusOverview />
+      
         <h1>Careers Page</h1>
         <Footer />
       </div>
