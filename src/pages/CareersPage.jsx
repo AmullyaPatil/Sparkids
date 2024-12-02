bef3529876a26049d3d96431dde35ca6adffad0c
AmullyaPatil <patilamullya@gmail.com>
2024-12-02 21:32:19 +0530

    abacus course pg

--- src/pages/CareersPage.jsx
+++ src/pages/CareersPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import AbacusOverview from '../components/AbacusOverview';
 import Footer from '../components/Footer';
 export const CareersPage = () => {
     return (
       <div>
         <Header />
+        <AbacusOverview />
         <h1>Careers Page</h1>
         <Footer />
       </div>
